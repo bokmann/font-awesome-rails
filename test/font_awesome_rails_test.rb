86b27004d5718796cb534c7fcab8fd88367fe24e
Ryan McGeary <ryan@mcgeary.org>
2013-02-10 14:01:35 -0500

    Added tests against font and stylesheet assets

--- test/font_awesome_rails_test.rb
+++ test/font_awesome_rails_test.rb
@@ -4,4 +4,30 @@ class FontAwesomeRailsTest < ActionDispatch::IntegrationTest
   test "engine is loaded" do
     assert_equal ::Rails::Engine, Font::Awesome::Rails::Engine.superclass
   end
+
+  test "fonts are served" do
+    get "/assets/fontawesome-webfont.eot"
+    assert_response :success
+    get "/assets/fontawesome-webfont.ttf"
+    assert_response :success
+    get "/assets/fontawesome-webfont.woff"
+    assert_response :success
+  end
+
+  test "stylesheets are served" do
+    get "/assets/font-awesome.css"
+    assert_response :success
+    get "/assets/font-awesome-ie7.min.css"
+    assert_response :success
+    get "/assets/font-awesome-ie7.css"
+    assert_response :success
+  end
+
+  test "stylesheets contain asset pipeline references to fonts" do
+    get "/assets/font-awesome.css"
+    assert_match "/assets/fontawesome-webfont.eot",  response.body
+    assert_match "/assets/fontawesome-webfont.eot?#iefix",  response.body
+    assert_match "/assets/fontawesome-webfont.woff", response.body
+    assert_match "/assets/fontawesome-webfont.ttf",  response.body
+  end
 end
