798ee70ac9465eed072f947afbc519c13c46534b
Ryan McGeary <ryan@mcgeary.org>
2013-03-10 13:26:06 -0400

    Only load rails engine if Rails is defined

--- lib/font-awesome-rails.rb
+++ lib/font-awesome-rails.rb
@@ -1,10 +1,2 @@
 require "font-awesome-rails/version"
-
-module Font
-  module Awesome
-    module Rails
-      class Engine < ::Rails::Engine
-      end
-    end
-  end
-end
+require "font-awesome-rails/engine" if defined?(::Rails)
---
+++ lib/font-awesome-rails/engine.rb
@@ -0,0 +1,8 @@
+module Font
+  module Awesome
+    module Rails
+      class Engine < ::Rails::Engine
+      end
+    end
+  end
+end
