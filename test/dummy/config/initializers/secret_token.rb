9c7e32a33c839f863d9c5b4a3caab0e48d03b15d
Ryan McGeary <ryan@mcgeary.org>
2013-04-30 15:43:16 -0400

    Eliminated deprecation warning while running the tests in Rails 4

--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '734142099bb34ad2a267bb95111490234357e4c7026be61a66b0c14343daa2ac44822fd513264d532fb39be241a7e06d598790641f241310794bcb5dda7c8ed4'
+Dummy::Application.config.secret_token    = 'deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef'
+Dummy::Application.config.secret_key_base = 'deadbeef' if Dummy::Application.config.respond_to?(:secret_key_base)
