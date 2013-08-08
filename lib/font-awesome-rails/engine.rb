d75ad82de4764f0197562633bea3162752c862b6
David Workman <me@workmad3.com>
2013-08-08 10:33:53 +0100

    Update engine.rb

--- lib/font-awesome-rails/engine.rb
+++ lib/font-awesome-rails/engine.rb
@@ -2,7 +2,7 @@ module FontAwesome
   module Rails
     class Engine < ::Rails::Engine
       config.to_prepare do
-        ApplicationController.helper(FontAwesome::Rails::Helpers::IconHelper)
+        ApplicationController.helper(FontAwesome::Rails::IconHelper)
       end
     end
   end
