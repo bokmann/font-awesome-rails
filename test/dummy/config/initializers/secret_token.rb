bf4359ffbdf193bb981ea2da2a46ed255ba7b721
Adam Constabaris <adjam@users.noreply.github.com>
2019-05-02 09:05:08 -0400

    set secret_key_base more responsibly in tests

--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -6,10 +6,17 @@
 # no regular words or you'll be exposed to dictionary attacks.
 #
 # avoid deprecation warnings if building against older versions of Rails
-SKB_VERSION = Gem::Version.new('4.0.0')
-RG_VERSION = Rails.respond_to?(:version) ? Gem::Version.new(Rails.version) : Gem::Version.new('3.22')
 
-if RG_VERSION < SKB_VERSION 
+# secret_token migrated to secret_key_base in Rails 4
+SKB_VERSION = Gem::Version.new('4.0.0')
+
+# Get the current Rails version.
+RAILS_VERSION = Rails.respond_to?(:version) ? Gem::Version.new(Rails.version) : Gem::Version.new('3.22')
+
+# if we're running an old version of Rails
+if RAILS_VERSION < SKB_VERSION 
   Dummy::Application.config.secret_token    = 'deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef'
+else
+  Dummy::Application.config.secret_key_base = 'deadbeef' if Dummy::Application.config.respond_to?(:secret_key_base)
 end 
-Dummy::Application.config.secret_key_base = 'deadbeef' if Dummy::Application.config.respond_to?(:secret_key_base)
+
