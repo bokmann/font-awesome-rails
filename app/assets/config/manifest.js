c1a7a9267ad90df486012fdb21d7a988c624ba60
Christopher Brown <chribrow@redhat.com>
2021-02-07 22:54:00 +0000

    Add a manifest file for Sprockets 3

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,9 @@
+// app/assets/config/manifest.js
+//
+//= link ../fonts/FontAwesome.otf
+//= link ../fonts/fontawesome-webfont.eot
+//= link ../fonts/fontawesome-webfont.svg
+//= link ../fonts/fontawesome-webfont.ttf
+//= link ../fonts/fontawesome-webfont.woff
+//= link ../fonts/fontawesome-webfont.woff2
+//= link_directory ../stylesheets .erb
